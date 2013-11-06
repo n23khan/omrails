37d1fc82f654664e5c2adb42cc8c5fe97a222599
Najeeb Khan <nk21@ymail.com>
2013-11-06 18:47:44 -0500

    Initial Pin Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = 'b5d05ad1c26ddae64560cd842378e497907c7da227ad9af6bc8c02945c100925be1901261fd3f3f1d35e848a2421739a8b2b968dedafc65ed1cd926cd8dcb233'
